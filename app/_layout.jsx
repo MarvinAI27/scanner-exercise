b018013e83af95b269c50b8c9eb441249d24d7f5
MarvinAI27 <marvin.fuisting@web.de>
2025-03-05 14:54:25 +0100

    add basic routing

---
+++ app/_layout.jsx
@@ -0,0 +1,19 @@
+import { Stack } from "expo-router";
+import { StyleSheet } from "react-native";
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ title: "Scanner" }} />
+      <Stack.Screen name="book-details" options={{ title: "Book details" }} />
+    </Stack>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
---
+++ app/book-details.jsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function BookDetails() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Book Details Page</Text>
+    </View>
+  );
+}
